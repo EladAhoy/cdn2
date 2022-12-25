74e357cb7a2a3fb2ac4ac9fcb2d64ab1f9e62b7e
Elad from Ahoy! <fiveminutes001@gmail.com>
2022-12-25 21:28:16 +0200

    with console.log

--- src/index.js
+++ src/index.js
@@ -14,8 +14,14 @@ root.render(
     <Content />
   </React.StrictMode>
 );
-
+console.log((
+  <div className="App">
+    <header className="App-header">
+      <img className="App-logo" alt="logo" />
+    </header>
+  </div>
+))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
