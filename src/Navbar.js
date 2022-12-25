eea27e3cb4070fcc73aeb891d9b8091ccac61bb8
Elad from Ahoy! <fiveminutes001@gmail.com>
2022-12-25 20:57:29 +0200

    changing to create-react-app

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (<nav className="navbar">
+    <h1>Example Site</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>);
+}
+
+export default Navbar;
