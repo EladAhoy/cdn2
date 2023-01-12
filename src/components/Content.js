e87942f0156da4bf02467c6da3f55c17e09cb00f
Elad from Ahoy! <fiveminutes001@gmail.com>
2023-01-12 11:21:15 +0200

    dropdown navbar

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,6 +5,7 @@ import BusinessCard from './BusinessCard';
 import BusinessCardLeft from './BusinessCardLeft';
 import ToastWithBackdrop from './ToastWithBackdrop';
 import CheckoutPage from './CheckoutPage';
+import Navbar from './DropDown';
 
 export default function Content() {
   const [showToast, setshowToast] = useState(true);
@@ -12,12 +13,218 @@ export default function Content() {
     const timer = setTimeout(() => setshowToast(false), 5800);
     return () => clearTimeout(timer);
   }, []);
+  const options = [
+    {
+      label: 'Project2',
+      children: [
+        {
+          label: 'About Us',
+          children: [
+            {
+              label: 'Our Team',
+              children: [
+                {
+                  label: 'John Smith',
+                },
+                {
+                  label: 'Jane Doe',
+                },
+              ],
+            },
+            {
+              label: 'Our History',
+            },
+          ],
+        },
+        {
+          label: 'Contact Us',
+          children: [
+            {
+              label: 'Email',
+              children: [
+                {
+                  label: 'support@example.com',
+                },
+                {
+                  label: 'sales@example.com',
+                },
+              ],
+            },
+            {
+              label: 'Phone',
+              children: [
+                {
+                  label: '+1 555 555 5555',
+                },
+                {
+                  label: '+1 555 555 5556',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'Products',
+      children: [
+        {
+          label: 'Category 1',
+          children: [
+            {
+              label: 'Product 1',
+            },
+            {
+              label: 'Product 2',
+            },
+          ],
+        },
+        {
+          label: 'Category 2',
+          children: [
+            {
+              label: 'Product 3',
+            },
+            {
+              label: 'Product 4',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'Blog',
+    },
+  ];
+
+  const optionsComplex = [
+    {
+      label: 'Project1',
+      children: [
+        {
+          label: 'About Us',
+          children: [
+            {
+              label: 'Our Story',
+              children: [
+                {
+                  label: 'Company History',
+                  children: [
+                    {
+                      label: 'Year 1',
+                      children: [
+                        {
+                          label: 'Milestone 1',
+                        },
+                        {
+                          label: 'Milestone 2',
+                        },
+                      ],
+                    },
+                    {
+                      label: 'Year 2',
+                      children: [
+                        {
+                          label: 'Milestone 3',
+                        },
+                        {
+                          label: 'Milestone 4',
+                        },
+                      ],
+                    },
+                  ],
+                },
+                {
+                  label: 'Our Vision',
+                  children: [
+                    {
+                      label: 'Mission Statement',
+                    },
+                    {
+                      label: 'Values',
+                      children: [
+                        {
+                          label: 'Integrity',
+                        },
+                        {
+                          label: 'Excellence',
+                        },
+                        {
+                          label: 'Innovation',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              label: 'Management Team',
+              children: [
+                {
+                  label: 'CEO',
+                  children: [
+                    {
+                      label: 'Bio',
+                    },
+                    {
+                      label: 'Contact',
+                      children: [
+                        {
+                          label: 'Email',
+                        },
+                        {
+                          label: 'Phone',
+                        },
+                      ],
+                    },
+                  ],
+                },
+                {
+                  label: 'CFO',
+                  children: [
+                    {
+                      label: 'Bio',
+                    },
+                    {
+                      label: 'Contact',
+                      children: [
+                        {
+                          label: 'Email',
+                        },
+                        {
+                          label: 'Phone',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: 'Contact Us',
+          children: [
+            {
+              label: 'Email',
+              children: [
+                {
+                  label: 'support@example.com',
+                }]
+            }]
+        }]
+    }, ...options]
+
   return (
     <main className="content">
       <ToastWithBackdrop show={showToast} />
       <section className='fun-facts'>
         <FunFacts />
       </section>
+      <section className='DropDown'>
+        <Navbar options={optionsComplex} />
+      </section> 
       <section className='shows'>
         <CheckoutPage />
       </section> 
---
+++ src/components/DropDown.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import "./DropDown.css";
+
+function DropdownMenu({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        {options.label}
+      </button>
+      {isOpen && (
+        <ul className='nested-dropdown-list'>
+          {options.children.map((child, index) => (
+            <li key={index}>
+              {child.children ? (
+                <DropdownMenu options={child} />
+              ) : (
+                <button>{child.label}</button>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+function Navbar({ options }) {
+  return (
+    <nav className='dropdwon-navbar'>
+      <h1>Dropdown Navbar</h1>
+      <ul>
+        {options.map((option, index) => (
+          <li key={index}>
+            {option.children ? (
+              <DropdownMenu options={option} />
+            ) : (
+              <button>{option.label}</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
