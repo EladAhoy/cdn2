f0db07bc919a7f2e9d85fc97175d17674798a810
DontWorryYouCanMerge <fiveminutes001@gmail.com>
2023-01-25 14:23:05 +0200

    remove test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Fullstack Developer', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Fullstack Developer/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders Fullstack Developer', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/Fullstack Developer/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
