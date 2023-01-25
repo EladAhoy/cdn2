5516c236591931b83fe20253835351fba51d7560
DontWorryYouCanMerge <fiveminutes001@gmail.com>
2023-01-25 14:17:04 +0200

    removing unused vars

--- src/components/SendTokens.js
+++ src/components/SendTokens.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import AddressValidationForm from './AddressValidationForm';
 import ConnectToMetamask from './ConnectToMetamask';
 import SendEthPayment from './SendEthPayment';
@@ -6,36 +5,6 @@ import './SendTokens.css';
 // import SendTokensEth from './SendTokensEth';
 
 export default function SendTokens() {
-  const [selectedShow] = useState();
-  const [quantity, setQuantity] = useState(1);
-  // const [ethAddress, setEthAddress] = useState();
-
-  const [finalCost, setFinalCost] = useState(0);
-  const [fees, setFees] = useState(0);
-
-  const handleQuantityChange = (event) => {
-    const amount = event.target.value;
-    if (!amount) return;
-    if (amount < 0) {
-      alert('Amount needs to be larger than 0');
-      setQuantity(1);
-      return;
-    };
-    setQuantity(event.target.value);
-  };
-
-  const calculateFinalCost = () => {
-    // Calculate the final cost based on the selected show, quantity, and fees
-    if (!selectedShow || selectedShow?.id === 0) {
-      setFinalCost(0);
-      setFees(0);
-      return;
-    }
-    const fees = 10 * quantity;
-    const showCost = selectedShow.cost * quantity;
-    setFinalCost(showCost + fees);
-    setFees(fees);
-  };
 
   const handleSubmit = (event) => {
     event?.preventDefault();
@@ -66,33 +35,7 @@ export default function SendTokens() {
             <p>Please provide a valid ETH address.</p>
             <AddressValidationForm />
           </fieldset>
-
-          {/* <fieldset className="show-details">
-            <h2>Token Amount</h2>
-            <p>Select amount to transfer.</p>
-            <label>
-              Amount:
-              <input type="number" value={quantity} onChange={handleQuantityChange} />
-            </label>
-          </fieldset> */}
-
-
-
         </section>
-        {/* <section>
-          <fieldset className="payment">
-            <h2>Pre Payment</h2>
-            <button type="button" onClick={calculateFinalCost}>
-              Calculate Fees
-            </button>
-          </fieldset>
-
-          <fieldset className="payment">
-            <h2>Total Cost: ${finalCost}</h2>
-            <p>Fees: ${fees}</p>
-            <button type="submit">Checkout</button>
-          </fieldset>
-        </section> */}
       </form>
     </div>
   );
