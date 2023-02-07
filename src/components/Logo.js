3d5ac67ef2155ab034702ce2220a32c110da1a2d
DontWorryYouCanMerge <fiveminutes001@gmail.com>
2023-02-07 14:41:47 +0200

    logo update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,11 @@
-import logo from '../img/logo.svg';
+// import logo from '../img/logo.svg';
+import shesdev from '../img/SHESDEV.png';
 import './Logo.css';
 
 export default function Logo() {
   return (
     <div className="logo-container">
-      <img src={logo} className="logo-container__App-logo" alt="logo" />     
+      <img src={shesdev} className="logo-container__App-logo" alt="logo" />     
     </div>
   );
 }
