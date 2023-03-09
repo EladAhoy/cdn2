378f897f656df9f14643effb7847062debafb6aa
DontWorryYouCanMerge <fiveminutes001@gmail.com>
2023-03-09 14:24:08 +0200

    compile successful, logo missing

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Logo.css";
 
 export default function Logo() {
-  const shesdevPath = "%PUBLIC_URL%/SHESDEV.png";
+  const shesdevPath = "../img/SHESDEV.png";
   return (
     <div className="logo-container">
       <img src={shesdevPath} className="logo-container__App-logo" alt="logo" />
