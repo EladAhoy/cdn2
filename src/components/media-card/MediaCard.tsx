cb8babc5c5d02d705f49958a9bae24ff0e6093bb
DontWorryYouCanMerge <fiveminutes001@gmail.com>
2023-03-24 15:20:19 +0300

    fixing iframe

--- src/components/media-card/MediaCard.tsx
+++ src/components/media-card/MediaCard.tsx
@@ -1,13 +1,15 @@
 import "./MediaCard.css";
-import CardMedia from "@mui/material/CardMedia";
 import React from "react";
 import { FixMeLater } from "../../types/general";
 
 export default function MediaCard({ link }: FixMeLater) {
   return (
-    <CardMedia
-      component="iframe"
+    <iframe
       src={link ?? "https://www.youtube.com/embed/Zk1NfO_LCFU"}
-    ></CardMedia>
+      width={1000}
+      height={500}
+      loading="eager"
+      title="A youtube video"
+    ></iframe>
   );
 }
