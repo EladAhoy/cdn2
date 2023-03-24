f75d4f4159314474bb3a18410c10b8dc5f08043e
DontWorryYouCanMerge <fiveminutes001@gmail.com>
2023-03-24 15:45:25 +0300

    with iframe loader

--- src/components/media-card/MediaCard.tsx
+++ src/components/media-card/MediaCard.tsx
@@ -1,15 +1,35 @@
 import "./MediaCard.css";
-import React from "react";
+import React, { useState } from "react";
 import { FixMeLater } from "../../types/general";
+var Spinner = require("react-spinkit");
 
 export default function MediaCard({ link }: FixMeLater) {
+  const [showPic, setShowPic] = useState(true);
+
+  const hideSpinner = () => {
+    setShowPic(false);
+  };
+
   return (
-    <iframe
-      src={link ?? "https://www.youtube.com/embed/Zk1NfO_LCFU"}
-      width={1000}
-      height={500}
-      loading="eager"
-      title="A youtube video"
-    ></iframe>
+    <>
+      <div className="container rsvp-wrapper">
+        {showPic ? (
+          <Spinner
+            className="loading text-center"
+            name="three-bounce"
+            color="white"
+            fadeIn="none"
+          />
+        ) : null}
+        <iframe
+          src={link ?? "https://www.youtube.com/embed/Zk1NfO_LCFU"}
+          width={1000}
+          height={500}
+          loading="eager"
+          title="A youtube video"
+          onLoad={hideSpinner}
+        ></iframe>
+      </div>
+    </>
   );
 }
